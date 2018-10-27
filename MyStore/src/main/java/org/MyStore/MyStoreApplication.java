f37ef9b993f44b58115c469fba3c6dfa9ee73fbe
exe <exe@DESKTOP-ALMN31U>
2018-10-27 21:30:43 +0100

    First Commit

---
+++ MyStore/src/main/java/org/MyStore/MyStoreApplication.java
@@ -0,0 +1,25 @@
+package org.MyStore;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStoreApplication implements CommandLineRunner {
+	
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MyStoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		
+	}
+}

a55c5edea146b48fa3a51d67368037b0b5f637b4
exe <exe@DESKTOP-ALMN31U>
2018-10-28 11:54:25 +0000

    add produit

---
+++ MyStore/src/main/java/org/MyStore/model/Produit.java
@@ -0,0 +1,14 @@
+package org.MyStore.model;
+
+import java.io.Serializable;
+
+public class Produit implements Serializable {
+	
+	private Long id;
+	private String ref;
+	private String name;
+	private String description;
+	private int quantite;
+	
+
+}
